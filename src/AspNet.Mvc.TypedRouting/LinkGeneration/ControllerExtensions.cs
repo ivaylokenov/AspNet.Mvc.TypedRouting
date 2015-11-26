741befeca0ffd4297ad38904d81db469d7bfd599
ivaylokenov <ivaylo.kenov@gmail.com>
2015-11-26 15:54:16 +0200

    Added ControllerExtensions

---
+++ src/AspNet.Mvc.TypedRouting/LinkGeneration/ControllerExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace AspNet.Mvc.TypedRouting.LinkGeneration
+{
+    using Microsoft.AspNet.Mvc;
+    using System;
+    using System.Linq.Expressions;
+
+    public static class ControllerExtensions
+    {
+        public static CreatedAtActionResult CreatedAtAction<TController>(
+            this TController controller,
+            Expression<Action<TController>> action,
+            object value)
+            where TController : Controller
+        {
+            return null;
+        }
+
+        public static CreatedAtActionResult CreatedAtAction<TController>(
+            this TController controller,
+            Expression<Action<TController>> action,
+            object routeValues,
+            object value)
+            where TController : Controller
+        {
+
+            return null;
+        }
+
+        public static CreatedAtActionResult CreatedAtAction<TRedirectController>(
+            this Controller controller,
+            Expression<Action<TRedirectController>> action,
+            object value)
+        {
+            return null;
+        }
+
+        public static CreatedAtActionResult CreatedAtAction<TRedirectController>(
+            this Controller controller,
+            Expression<Action<TRedirectController>> action,
+            object routeValues,
+            object value)
+        {
+            return null;
+        }
+    }
+}
