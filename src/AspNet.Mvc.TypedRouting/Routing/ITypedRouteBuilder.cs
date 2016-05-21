56eac2d361411b5676aa57d378b44cba8c0f3873
apxoht <ivaylo.kenov@gmail.com>
2016-05-21 22:10:28 +0300

    Added documentation on ITypedRouteBuilder (closes #6)

--- src/AspNet.Mvc.TypedRouting/Routing/ITypedRouteBuilder.cs
+++ src/AspNet.Mvc.TypedRouting/Routing/ITypedRouteBuilder.cs
@@ -4,14 +4,44 @@
 
     public interface ITypedRouteBuilder
     {
+        /// <summary>
+        /// Adds HTTP GET method route by the provided template. 
+        /// </summary>
+        /// <param name="template">Route template.</param>
+        /// <param name="configuration">Route configuration.</param>
+        /// <returns>Typed route.</returns>
         TypedRoute Get(string template, Action<TypedRoute> configuration);
 
+        /// <summary>
+        /// Adds HTTP POST method route by the provided template. 
+        /// </summary>
+        /// <param name="template">Route template.</param>
+        /// <param name="configuration">Route configuration.</param>
+        /// <returns>Typed route.</returns>
         TypedRoute Post(string template, Action<TypedRoute> configuration);
 
+        /// <summary>
+        /// Adds HTTP PUT method route by the provided template. 
+        /// </summary>
+        /// <param name="template">Route template.</param>
+        /// <param name="configuration">Route configuration.</param>
+        /// <returns>Typed route.</returns>
         TypedRoute Put(string template, Action<TypedRoute> configuration);
 
+        /// <summary>
+        /// Adds HTTP DELETE method route by the provided template. 
+        /// </summary>
+        /// <param name="template">Route template.</param>
+        /// <param name="configuration">Route configuration.</param>
+        /// <returns>Typed route.</returns>
         TypedRoute Delete(string template, Action<TypedRoute> configuration);
 
+        /// <summary>
+        /// Adds typed route by the provided template. 
+        /// </summary>
+        /// <param name="template">Route template.</param>
+        /// <param name="configuration">Route configuration.</param>
+        /// <returns>Typed route.</returns>
         TypedRoute Add(string template, Action<TypedRoute> configuration);
     }
 }
